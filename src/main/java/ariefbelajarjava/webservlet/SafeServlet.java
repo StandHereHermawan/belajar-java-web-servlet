4ae505cd822ca573aff3dceaaa61603a06e16fbf
StandHereHermawan <nolifegansaya@gmail.com>
2023-07-04 09:07:20 +0700

    Belajar Java Web Servlet, Praktek Servlet yang tidak sharing variable response

---
+++ src/main/java/ariefbelajarjava/webservlet/SafeServlet.java
@@ -0,0 +1,29 @@
+package ariefbelajarjava.webservlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/unsafe")
+public class SafeServlet extends HttpServlet {
+
+    // tidak sharing variable response, kemungkinan penyebab race condition
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        Long sleep = Long.parseLong(req.getParameter("sleep"));
+
+        String response = "Hello " + name;
+        try {
+            Thread.sleep(sleep);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        resp.getWriter().println(response);
+    }
+}
