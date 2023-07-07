ded41e0dd46711b8866ebf3f51f44a5952e517aa
StandHereHermawan <nolifegansaya@gmail.com>
2023-07-07 17:04:35 +0700

    Belajar Java Web Servlet, Form Request Fixed coy wkwkwkw

--- src/main/java/ariefbelajarjava/webservlet/FormServlet.java
+++ src/main/java/ariefbelajarjava/webservlet/FormServlet.java
@@ -7,17 +7,32 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 @WebServlet(urlPatterns = "/form")
 public class FormServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Path path = Path.of(FormServlet.class.getResource("/html/form.html").getPath());
-        String html = Files.readString(path);
-        resp.getWriter().println(html);
+        Path path = Path.of("src/main/resources/html/form.html");
+
+        try (InputStream inputStream = Files.newInputStream(path);
+             Scanner scanner = new Scanner(inputStream)) {
+
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                // System.out.println(line);
+                resp.getWriter().println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Path path = Path.of("form.html");
+        // String html = Files.readString(path);
     }
 
     @Override
