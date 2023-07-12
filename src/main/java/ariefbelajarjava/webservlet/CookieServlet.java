96291845571696fac5d07779379f1c1212e066cf
StandHereHermawan <nolifegansaya@gmail.com>
2023-07-12 19:59:55 +0700

    Belajar Java Web Servlet, Cookie, method Post mengirim cookie

---
+++ src/main/java/ariefbelajarjava/webservlet/CookieServlet.java
@@ -0,0 +1,26 @@
+package ariefbelajarjava.webservlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/cookie")
+public class CookieServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String cookieName = req.getParameter("name");
+        String cookieValue = req.getParameter("value");
+
+        Cookie cookie = new Cookie(cookieName, cookieValue);
+        cookie.setPath("/");
+
+        resp.addCookie(cookie);
+        resp.getWriter().println("Success add cookie " + cookieName + ":" + cookieValue);
+    }
+}
